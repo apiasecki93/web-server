93d19becbfb1ac4a1e50fc64513797517e729e8d
Artur Piasecki <apiasecki93@gmail.com>
2022-05-13 13:02:33 +0200

    ready for Heroku deploy

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const forecast = require('../src/utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for Express config
 const publicDirecotryPath = path.join(__dirname, '../public')
@@ -127,6 +128,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
 })
